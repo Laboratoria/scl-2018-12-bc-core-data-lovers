6a48d3fb1261066e0a7f3deb68cc60dbb42b51be
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-01-08 12:29:17 -0300

    V2.3 data.JS: Cambia forma de llamar a la data

--- src/data.js
+++ src/data.js
@@ -1,17 +1,27 @@
+let totalData= "";
 
-window.pokemonData = {
- 
-  filter: (data) => {
+fetch("pokemon.json")
+            .then(data => data.json())
+            .then(data => {
+              
+              totalData=data
+     }),
+
+window.totalData= totalData
+
+window.pokemones = {
+
+  pokeFilter: (data,condition) => {
     
-    const result = data.pokemon.filter(element => {
+    const result = data.filter(element => {
       return element.type.indexOf(condition) !== -1 ;
     })
     return result
    },
-  
- 
+
+   
   orderAZ: (data) => {
-    const resultOrden = data.pokemon.sort((a, b) => {
+    const resultOrden = data.sort((a, b) => {
       if (a.name > b.name) {
           return 1;
       } if (a.name < b.name) {
@@ -21,8 +31,8 @@ window.pokemonData = {
           return resultOrden
         },
  
-   orderZA: (data) => {
-          const resultOrden = data.pokemon.sort((a, b) => {
+  orderZA: (data) => {
+          const resultOrden = data.sort((a, b) => {
             if (a.name < b.name) {
                 return 1;
             } if (a.name > b.name) {
